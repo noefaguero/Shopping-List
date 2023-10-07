1977e427b1b1ac71cff32d4cc981bfc3e5f3d605
Noé Fernández Agüero <noefernandezaguero@MacBook-Pro-de-Noe.local>
2023-10-07 10:44:29 +0200

    Initial commit (static design)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const nuevoalimento = document.getElementById("nuevoalimento");
+const list = document.getElementById("list");
+const btnaniadir = document.getElementById("btnaniadir");
+const btncomprar = document.getElementById("btncomprar");
+const error = document.getElementById("error");
+const total = document.getElementById("total");
+
